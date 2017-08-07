5e109fceba55d16a12870c453bb8cb1460e19617
Theo Turner <theo@Theos-MacBook-Pro.local>
2017-08-07 13:43:46 +0100

    Version 0.4 Collision detection working

--- Retrofire/GameScene.swift
+++ Retrofire/GameScene.swift
@@ -14,89 +14,86 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var sand:SKEmitterNode!
     var player:SKSpriteNode!
-    
     var scoreLabel:SKLabelNode!
     var score:Int = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
         }
     }
-    
     var gameTimer:Timer!
+    var sceneryList = ["destructible-1", "destructible-2", "destructible-3", "destructible-4", "destructible-5", "destructible-6", "indestructible-1", "indestructible-2", "indestructible-3", "indestructible-4"]
     
-    var possibleScenery = ["destructible-1", "destructible-2", "destructible-3", "destructible-4", "destructible-5", "destructible-6", "indestructible-1", "indestructible-2", "indestructible-3", "indestructible-4"]
+    let sceneryCategory:UInt32 = 0x1 << 1
+    let bulletCategory:UInt32 = 0x1 << 0
     
-    let sceneryCategory:UInt32 = 0x1 << 1 // Bitshift allows easy unique identifiers
-    let bulletCategory:UInt32 = 0x1 << 0 // Different shift to keep uniqueness
-    
-    let motionManager = CMMotionManager()
+    let motionManger = CMMotionManager()
     var xAcceleration:CGFloat = 0
     
     override func didMove(to view: SKView) {
         
-        // ANCHOR POINT IS CENTER BOTTOM (x: center, y: 0.1 * screen height)
-        // Determines player position and is defined in GameScene.sks
-        
         sand = SKEmitterNode(fileNamed: "Sand")
-        sand.position = CGPoint(x:0, y: 0.9 * self.size.height)
-        sand.advanceSimulationTime(20)
+        sand.position = CGPoint(x: self.size.width / 2, y: self.size.height)
+        sand.advanceSimulationTime(10)
+        
         self.addChild(sand)
+        
         sand.zPosition = -1
         
         player = SKSpriteNode(imageNamed: "player-green")
-        player.texture!.filteringMode = .nearest // For pixelated look
-        player.setScale(10) // Must scale sprites after physicsBody declarations
+        player.position = CGPoint(x: self.size.width / 2, y: 0.1 * self.size.height)
+        player.texture!.filteringMode = .nearest
+        player.setScale(8)
+        
         self.addChild(player)
         
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         self.physicsWorld.contactDelegate = self
         
         scoreLabel = SKLabelNode(text: "Score: 0")
-        scoreLabel.position = CGPoint(x: -0.35 * self.size.width, y: 0.85 * self.size.height) // Change as wanted
-        scoreLabel.fontName = "AmericanTypewriter-Bold" // Change as wanted
-        scoreLabel.fontSize = 36 // Change as wanted
-        scoreLabel.fontColor = UIColor.black // Change as wanted - if white (default), remove line
+        scoreLabel.position = CGPoint(x: 100, y: self.size.height - 60)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.black
         score = 0
+        
         self.addChild(scoreLabel)
         
-        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addScenery), userInfo: nil, repeats: true) // Frequency of adding scenery (time interval in seconds), increase for higher difficulty
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addScenery), userInfo: nil, repeats: true)
         
-        motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
+        motionManger.accelerometerUpdateInterval = 0.2
+        motionManger.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
             if let accelerometerData = data {
                 let acceleration = accelerometerData.acceleration
-                self.xAcceleration = 0.75 * CGFloat(acceleration.x) + 0.25 * self.xAcceleration // Change as wanted
+                self.xAcceleration = CGFloat(acceleration.x) * 0.75 + self.xAcceleration * 0.25
             }
         }
         
-        }
+    }
     
     func addScenery () {
         
-        possibleScenery = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleScenery) as! [String]
-        let scenery = SKSpriteNode(imageNamed: possibleScenery[0])
+        sceneryList = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: sceneryList) as! [String]
+        let scenery = SKSpriteNode(imageNamed: sceneryList[0])
+        let generateSceneryPosition = GKRandomDistribution(lowestValue: 0, highestValue: Int(self.size.width))
+        let position = CGFloat(generateSceneryPosition.nextInt())
         
-        let generatePosition = GKRandomDistribution(lowestValue: -Int(self.size.width) / 2, highestValue: Int(self.size.width) / 2)
-        let position = CGFloat(generatePosition.nextInt())
-        scenery.position = CGPoint(x: position, y: 0.9 * self.size.height + 6 * scenery.size.height) // Multiplied by 6 as this declaration before setting scenery scale to 6
-        
+        scenery.position = CGPoint(x: position, y: self.size.height + 8 * scenery.size.height) // Multiplier must match scenery.setScale below
         scenery.physicsBody = SKPhysicsBody(rectangleOf: scenery.size)
         scenery.physicsBody?.isDynamic = true
         scenery.physicsBody?.categoryBitMask = sceneryCategory
         scenery.physicsBody?.contactTestBitMask = bulletCategory
         scenery.physicsBody?.collisionBitMask = 0
-        
-        scenery.texture!.filteringMode = .nearest // For pixelated look
-        scenery.setScale(6) // Must scale sprites after physicsBody declarations
+        scenery.texture!.filteringMode = .nearest
+        scenery.setScale(8)
         
         self.addChild(scenery)
         
-        let animationDuration:TimeInterval = 10 // Speed of scenery, decrease for higher difficulty - MUST MATCH BACKGROUND SPEED (animationDuration 10 == Sand.sks speed 152.8)
+        let animationDuration:TimeInterval = 10 // 10 matches speed 159.2 at scale 8
         var actionArray = [SKAction]()
-        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -0.1 * self.size.height - scenery.size.height), duration: animationDuration))
-        actionArray.append(SKAction.removeFromParent()) // Removes once off screen to save memory - see that the number of nodes is not constantly increasing
+        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -scenery.size.height), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
         scenery.run(SKAction.sequence(actionArray))
-    
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -112,26 +109,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bulletNode = SKSpriteNode(imageNamed: "bullet")
         
         bulletNode.position = player.position
-        bulletNode.position.y += (player.size.height + bulletNode.size.height) / 2
-        
-        bulletNode.physicsBody = SKPhysicsBody(rectangleOf: bulletNode.size) // BULLET HITBOX
+        bulletNode.position.y += player.size.height / 2
+        bulletNode.physicsBody = SKPhysicsBody(rectangleOf: bulletNode.size)
         bulletNode.physicsBody?.isDynamic = true
         bulletNode.physicsBody?.categoryBitMask = bulletCategory
         bulletNode.physicsBody?.contactTestBitMask = sceneryCategory
         bulletNode.physicsBody?.collisionBitMask = 0
         bulletNode.physicsBody?.usesPreciseCollisionDetection = true
-        
-        bulletNode.setScale(12)
+        bulletNode.texture!.filteringMode = .nearest // Remove line if keeping art a uniform colour rectangle
+        bulletNode.setScale(8)
         
         self.addChild(bulletNode)
         
-        let animationDuration:TimeInterval = 1 // Speed of bullet - possible improvement: calculate based on screensize
+        let animationDuration:TimeInterval = 0.3
         var actionArray = [SKAction]()
-        actionArray.append(SKAction.move(to: CGPoint(x: 0, y: 0.95 * self.size.height), duration: animationDuration)) // X must be player position, in this case it is 0 due to our anchor point
-        actionArray.append(SKAction.removeFromParent()) // Removes once off screen to save memory - see that the number of nodes is not constantly increasing
+        actionArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: self.size.height + bulletNode.size.height), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
         bulletNode.run(SKAction.sequence(actionArray))
         
     }
+    
     
     func didBegin(_ contact: SKPhysicsContact) {
         
@@ -141,18 +138,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
             firstBody = contact.bodyA
             secondBody = contact.bodyB
-        } else {
-            secondBody = contact.bodyB
-            firstBody = contact.bodyA
+        }
+        else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
         }
         
         if firstBody.categoryBitMask & bulletCategory != 0 && secondBody.categoryBitMask & sceneryCategory != 0 {
-            bulletDidCollideWithScenery(bulletNode: firstBody.node as! SKSpriteNode, sceneryNode: secondBody.node as! SKSpriteNode)
+            bulletHitScenery(bulletNode: firstBody.node as! SKSpriteNode, sceneryNode: secondBody.node as! SKSpriteNode)
         }
         
     }
     
-    func bulletDidCollideWithScenery (bulletNode:SKSpriteNode, sceneryNode:SKSpriteNode) {
+    
+    func bulletHitScenery (bulletNode:SKSpriteNode, sceneryNode:SKSpriteNode) {
         
         let explosion = SKEmitterNode(fileNamed: "Explosion")!
         
@@ -164,22 +163,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         bulletNode.removeFromParent()
         sceneryNode.removeFromParent()
+        
         self.run(SKAction.wait(forDuration: 2)) {
             explosion.removeFromParent()
         }
         
-        score += 5 // Change as wanted
+        score += 5
         
     }
     
     override func didSimulatePhysics() {
         
-        player.position.x += 50 * xAcceleration // Change as wanted
+        player.position.x += xAcceleration * 50
         
-        if player.position.x < -self.size.width / 2 - player.size.width {
-            player.position = CGPoint(x: self.size.width / 2 + player.size.width, y: player.position.y)
-        } else if player.position.x > self.size.width / 2 + player.size.width {
-            player.position = CGPoint(x: -self.size.width / 2 - player.size.width, y: player.position.y)
+        if player.position.x < -player.size.width {
+            player.position = CGPoint(x: self.size.width + player.size.width, y: player.position.y)
+        }
+        else if player.position.x > self.size.width + player.size.width {
+            player.position = CGPoint(x: -player.size.width, y: player.position.y)
         }
         
     }
